146796f8769d582e53732ba049df6c5bb6c5badc
Lala Sabathil <aiko@aitsys.dev>
2022-12-13 05:15:35 +0100

    Add note about boolean_neq (8) (#10)

--- src/register.js
+++ src/register.js
@@ -6,7 +6,7 @@ import config from './config.js';
  * Note: uses a Bot token for authentication, not a user token.
  */
 const url = `https://discord.com/api/v10/applications/${config.DISCORD_CLIENT_ID}/role-connections/metadata`;
-// supported types: number_lt=1, number_gt=2, number_eq=3 number_neq=4, datetime_lt=5, datetime_gt=6, boolean_eq=7
+// supported types: number_lt=1, number_gt=2, number_eq=3 number_neq=4, datetime_lt=5, datetime_gt=6, boolean_eq=7, boolean_neq=8
 const body = [
   {
     key: 'cookieseaten',
